ce849f02d1ca9f7523ed45a58fa6cb7b89d982bb
Myles Grant <myles@mylesgrant.com>
2021-01-11 22:41:40 -0800

    Actually drawing stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // https://github.com/ggez/ggez/blob/master/docs/FAQ.md#i-get-a-console-window-when-i-launch-my-executable-on-windows
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use ggez::event::{self, EventHandler};
+use ggez::nalgebra as na;
 use ggez::{graphics, timer, Context, ContextBuilder, GameResult};
 
 fn main() {
@@ -12,10 +13,10 @@ fn main() {
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
     // use when setting your game up.
-    let mut my_game = SpaceStationGodGame::new(&mut ctx);
+    let mut state = SpaceStationGodGame::new(&mut ctx);
 
     // Run!
-    match event::run(&mut ctx, &mut event_loop, &mut my_game) {
+    match event::run(&mut ctx, &mut event_loop, &mut state) {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured: {}", e),
     }
@@ -24,6 +25,7 @@ fn main() {
 // Main game state object. Holds positions, scores, etc
 struct SpaceStationGodGame {
     dt: std::time::Duration,
+    circle_pos_x: f32,
 }
 
 impl SpaceStationGodGame {
@@ -31,6 +33,7 @@ impl SpaceStationGodGame {
         // Load/create resources such as images here and otherwise initialize state
         SpaceStationGodGame {
             dt: std::time::Duration::new(0, 0),
+            circle_pos_x: 0.0,
         }
     }
 }
@@ -42,13 +45,32 @@ impl EventHandler for SpaceStationGodGame {
     // Returns GameResult so ggez can handle any errors
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         self.dt = timer::delta(ctx);
+        self.circle_pos_x = self.circle_pos_x % 800.0 + 1.0; // Move the circle to the right and wrap around when the end is reached
         Ok(())
     }
 
     // Draw updates this loop
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
-        graphics::clear(ctx, graphics::WHITE);
-        graphics::present(ctx)
+
+        // Draw a blue background
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        // Create a white filled circle
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            na::Point2::new(0.0, 0.0),
+            100.0,
+            2.0,
+            graphics::WHITE,
+        )?;
+        // Draw the circle at the position
+        graphics::draw(ctx, &circle, (na::Point2::new(self.circle_pos_x, 380.0),))?;
+
+        // Actually draw everything to the screen
+        graphics::present(ctx)?;
+
+        Ok(())
     }
 }
