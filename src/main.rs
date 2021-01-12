de6dded2f5be5612cca722735d6571d96daeefc5
Myles Grant <myles@mylesgrant.com>
2021-01-11 23:10:24 -0800

    Draw text on the screen

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 // https://github.com/ggez/ggez/blob/master/docs/FAQ.md#i-get-a-console-window-when-i-launch-my-executable-on-windows
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use ggez::event::{self, EventHandler};
+use ggez::mint;
 use ggez::nalgebra as na;
 use ggez::{graphics, timer, Context, ContextBuilder, GameResult};
+use std::env;
+use std::path;
 
 // Main game state object. Holds positions, scores, etc
 struct SpaceStationGodGame {
-    dt: std::time::Duration,
-    circle_pos_x: f32,
+    dt: std::time::Duration, // Time between updates
+    circle_pos_x: f32,       // Position of a circle on the screen
+    frames: usize,           // Total number of frames drawn
+    text: graphics::Text,    // Some text to draw on the screen
 }
 
 impl SpaceStationGodGame {
-    pub fn new(_ctx: &mut Context) -> SpaceStationGodGame {
+    pub fn new(ctx: &mut Context) -> GameResult<SpaceStationGodGame> {
+        // The ttf file will be in your resources directory. Later, we
+        // will mount that directory so we can omit it in the path here.
+        let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf")?;
+        let text = graphics::Text::new(("Hello world!", font, 48.0));
+
         // Load/create resources such as images here and otherwise initialize state
-        SpaceStationGodGame {
+        let s = SpaceStationGodGame {
             dt: std::time::Duration::new(0, 0),
             circle_pos_x: 0.0,
-        }
+            frames: 0,
+            text: text,
+        };
+
+        Ok(s)
     }
 }
 
@@ -33,8 +47,6 @@ impl EventHandler for SpaceStationGodGame {
 
     // Draw updates this loop
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        println!("Hello ggez! dt = {}ns", self.dt.subsec_nanos());
-
         // Draw a blue background
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
@@ -50,28 +62,56 @@ impl EventHandler for SpaceStationGodGame {
         // Draw the circle at the position
         graphics::draw(ctx, &circle, (na::Point2::new(self.circle_pos_x, 380.0),))?;
 
+        // Drawables are drawn from their top-left corner.
+        // Draw some text moving from top-left to bottom-right
+        let offset = self.frames as f32 / 10.0;
+        let dest_point = mint::Point2 {
+            x: (offset),
+            y: (offset),
+        };
+        graphics::draw(ctx, &self.text, (dest_point,))?;
+
         // Actually draw everything to the screen
         graphics::present(ctx)?;
+
+        // Increment the number of frames drawn
+        self.frames += 1;
+        if (self.frames % 100) == 0 {
+            // Every 100 frames print some stats to the console
+            println!(
+                "FPS: {}, dt: {}ns",
+                ggez::timer::fps(ctx),
+                self.dt.subsec_nanos()
+            );
+        }
 
         Ok(())
     }
 }
 
 // Entrypoint
-fn main() {
+fn main() -> GameResult {
+    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
+    // so that ggez will look in our cargo project directory for files.
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
+    };
+
     // Make a Context. This is passed to the game loop
     let (mut ctx, mut event_loop) = ContextBuilder::new("space_station_god_game", "Myles Grant")
+        .add_resource_path(resource_dir)
         .build()
         .expect("aieee, could not create ggez context!");
 
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
     // use when setting your game up.
-    let mut state = SpaceStationGodGame::new(&mut ctx);
+    let mut state = SpaceStationGodGame::new(&mut ctx)?;
 
     // Run!
-    match event::run(&mut ctx, &mut event_loop, &mut state) {
-        Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e),
-    }
+    event::run(&mut ctx, &mut event_loop, &mut state)
 }
