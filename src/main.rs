e02d78284ff204a5df6a50fd4fc8b68c1fe0d04e
Myles Grant <myles@mylesgrant.com>
2021-01-13 12:38:03 -0800

    Smoother circle

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ impl EventHandler for SpaceStationGodGame {
             graphics::DrawMode::fill(),
             na::Point2::new(0.0, 0.0),
             100.0,
-            2.0,
+            0.1,
             graphics::WHITE,
         )?;
         // Draw the circle at the position
